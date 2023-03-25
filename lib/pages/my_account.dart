2ee5ee94b346678b21152a479499e4f5cf2924d8
--get <67471674+ninebolt6@users.noreply.github.com>
2023-03-25 22:01:22 +0900

    feat: show email address

--- lib/pages/my_account.dart
+++ lib/pages/my_account.dart
@@ -10,7 +10,7 @@ class MyAccountPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authController = ref.watch(authControllerProvider);
+    final firebaseAuth = ref.watch(firebaseAuthProvider);
     return Scaffold(
       appBar: commonAppBar(),
       bottomNavigationBar: const CommonNavigationBar(),
@@ -34,11 +34,7 @@ class MyAccountPage extends ConsumerWidget {
                 ),
               ),
               SizedBox(
-                width: 461.9,
-                height: 236.8,
-                // decoration: BoxDecoration(
-                //   color: Theme.of(context).secondaryBackground,
-                // ),
+                width: 460,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -46,45 +42,25 @@ class MyAccountPage extends ConsumerWidget {
                       padding:
                           const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
                       child: TextFormField(
+                        initialValue: firebaseAuth.currentUser?.email ?? '',
                         decoration: InputDecoration(
                           labelText: 'Email',
                           // labelStyle: Theme.of(context).subtitle2,
-                          hintText: 'Please enter your full name...',
-                          // hintStyle: Theme.of(context).subtitle2,
                           enabledBorder: OutlineInputBorder(
                             borderSide: BorderSide(
                               color: Theme.of(context).colorScheme.background,
                             ),
                             borderRadius: BorderRadius.circular(8),
                           ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Color(0x00000000),
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          errorBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Color(0x00000000),
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          focusedErrorBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Color(0x00000000),
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
                           filled: true,
                           fillColor: Theme.of(context).colorScheme.background,
                           prefixIcon: const Icon(
-                            Icons.person_rounded,
+                            Icons.email,
                             // color: Theme.of(context).secondaryText,
                           ),
                         ),
+                        enabled: false,
                         style: Theme.of(context).textTheme.bodyMedium,
-                        // validator:
-                        //     _model.textController1Validator.asValidator(context),
                       ),
                     ),
                     Padding(
@@ -134,52 +110,6 @@ class MyAccountPage extends ConsumerWidget {
                         style: Theme.of(context).textTheme.bodyMedium,
                         // validator:
                         //     _model.textController2Validator.asValidator(context),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
-                      child: TextFormField(
-                        // controller: _model.textController3,
-                        decoration: InputDecoration(
-                          labelText: 'user name',
-                          // labelStyle: Theme.of(context).subtitle2,
-                          hintText: 'Please enter your full name...',
-                          // hintStyle: Theme.of(context).subtitle2,
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Theme.of(context).colorScheme.background,
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Color(0x00000000),
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          errorBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Color(0x00000000),
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          focusedErrorBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Color(0x00000000),
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          filled: true,
-                          fillColor: Theme.of(context).colorScheme.background,
-                          prefixIcon: const Icon(
-                            Icons.person_rounded,
-                            // color: Theme.of(context).secondaryText,
-                          ),
-                        ),
-                        style: Theme.of(context).textTheme.bodyMedium,
-                        // validator:
-                        //     _model.textController3Validator.asValidator(context),
                       ),
                     ),
                   ],
