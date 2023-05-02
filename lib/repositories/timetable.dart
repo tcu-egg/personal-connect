88ddff5ebba028d6b32a2e902eeb2263855afc84
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-02 17:17:00 +0900

    feat: create timetable repository

---
+++ lib/repositories/timetable.dart
@@ -0,0 +1,39 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../controllers/store.dart';
+import '../entity/timetable/timetable.dart';
+import '../utils/date.dart';
+
+final _timetableStoreProvider = Provider.family((ref, String userId) {
+  final store = ref.watch(firestoreProvider);
+  final timetablesRef = store.collection('timetables');
+  return timetablesRef.doc(userId);
+});
+
+final timetableStreamProvider =
+    StreamProvider.autoDispose.family<Timetable, String>((ref, userId) {
+  final store = ref.watch(_timetableStoreProvider(userId));
+  final res = store.snapshots().map((doc) => doc.data());
+
+  return res.map((data) {
+    if (data == null) {
+      return Timetable.withDefaults();
+    }
+    return Timetable.fromJson(data);
+  });
+});
+
+final timetableRepositoryProvider = Provider.family(TimetableRepository.new);
+
+class TimetableRepository {
+  TimetableRepository(this.ref, this.userId);
+  final ProviderRef<TimetableRepository> ref;
+  final String userId;
+
+  Future<void> save({required Timetable entity}) async {
+    final store = ref.watch(_timetableStoreProvider(userId));
+    await store.set(
+      entity.copyWith(updatedAt: timeNow()).toJson(),
+    );
+  }
+}
