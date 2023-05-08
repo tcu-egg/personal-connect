b7503a99108ee448b7a0d9e380c69ff11f100316
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-08 22:57:51 +0900

    feat: logout button

--- lib/common/widgets/app_bar.dart
+++ lib/common/widgets/app_bar.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CommonAppBar extends StatelessWidget implements PreferredSizeWidget {
+import '../../external/controllers/auth.dart';
+
+class CommonAppBar extends ConsumerWidget implements PreferredSizeWidget {
   const CommonAppBar({super.key, this.text = 'Personal Connect'});
   final String text;
 
@@ -8,13 +12,52 @@ class CommonAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return AppBar(
       backgroundColor: const Color(0xFF6DB1E7),
       automaticallyImplyLeading: false,
       title: Text(text),
       centerTitle: false,
       elevation: 2,
+      actions: [
+        IconButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (_) {
+                return AlertDialog(
+                  title: const Text('ログアウト'),
+                  content: const Text('ログアウトしますか？'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      style: const ButtonStyle(
+                        elevation: MaterialStatePropertyAll(0),
+                      ),
+                      child: const Text('キャンセル'),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final authController = ref.read(authControllerProvider);
+                        await authController.signOut();
+                        if (context.mounted) {
+                          Navigator.pop(context);
+                          GoRouter.of(context).go('/');
+                        }
+                      },
+                      style: const ButtonStyle(
+                        elevation: MaterialStatePropertyAll(0),
+                      ),
+                      child: const Text('はい'),
+                    )
+                  ],
+                );
+              },
+            );
+          },
+          icon: const Icon(Icons.logout_rounded),
+        ),
+      ],
     );
   }
 }
