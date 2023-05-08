cc2e3e32c351b3abd6e195ca2c3351819a901170
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-08 22:38:18 +0900

    feat: Gender.toString()

--- lib/user_data/entities/gender.dart
+++ lib/user_data/entities/gender.dart
@@ -3,4 +3,18 @@ enum Gender {
   male,
   female,
   other;
+
+  @override
+  String toString() {
+    switch (this) {
+      case Gender.none:
+        return '未設定';
+      case Gender.male:
+        return '男';
+      case Gender.female:
+        return '女';
+      case Gender.other:
+        return 'その他';
+    }
+  }
 }
